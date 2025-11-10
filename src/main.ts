7d34101f7c9b4240fe783049e0f7feb1a5e71405
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-11-09 18:52:12 -0600

    chore(main): config necessary deps

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
+import { Drivers } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -9,6 +13,13 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      IonicStorageModule.forRoot({
+        name: '__moviedb',
+        driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+      })
+    )
   ],
 });
