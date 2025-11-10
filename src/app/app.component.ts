f77d1430514e95eb3eceab9df8a88eb2e9cef641
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-11-09 18:53:38 -0600

    feat(app): config tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { film, search, star } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+  imports: [IonApp, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons({ film, search, star });
+  }
 }
